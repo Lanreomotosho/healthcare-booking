98c9b616808c6cffba210e635751e69ec6819e3a
Lanre Omotosho <omotoshoolanrewaju@gmail.com>
2024-04-25 02:45:35 +0100

    first commit

---
+++ src/pages/Doctors/DoctorsDetails.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const DoctorsDetails = () => {
+  return (
+    <div>DoctorsDetails</div>
+  )
+}
+
+export default DoctorsDetails;
--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import Home from "../pages/Home";
+import React from 'react';
+import Services from '../pages/Services';
+import Login from '../pages/Login';
+import Signup from '../pages/Signup';
+import Contact from '../pages/Contact';
+import Doctors from '../pages/Doctors/Doctors';
+import DoctorsDetails from '../pages/Doctors/DoctorsDetails';
 
+
+
+import {Routes, Route} from 'react-router-dom'
 const Routers = () => {
-  return (
-    <div>Routers</div>
-  )
-}
+  return <Routes>
+    <Routes path="/" element={<Home/>} />
+    <Routes path="/home" element={<Home/>} />
+    <Routes path="/doctors" element={<Doctors/>} />
+    <Routes path="/doctors/:id" element={<DonctorDetails/>} />
+    <Routes path="/login" element={<Login/>} />
+    <Routes path="/register" element={<Signup/>} />
+    <Routes path="/contact" element={<Contact/>} />
+    <Routes path="/services" element={<Services/>} />
+  </Routes>
+};
 
 export default Routers;
