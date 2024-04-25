21418c67a75ab3eceadb1579e03f2b97bf926b5c
Lanre Omotosho <omotoshoolanrewaju@gmail.com>
2024-04-25 17:47:00 +0100

    first commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
-import {useEffect, useState} from 'react'
+import {useEffect,useRef} from 'react';
 import logo from '../../assets/images/logo.png';
-import { NavLink, Link } from 'react-router-dom';
+import userImg from '../../assets/images/avatar-icon.png';
+import {NavLink, Link } from 'react-router-dom';
+
 
 
 const navLinks = [
@@ -9,7 +11,7 @@ const navLinks = [
     display:'Home'
   },
   {
-    path:'/doctor',
+    path:'/doctors',
     display:'Find a Doctor'
   },
   {
@@ -24,7 +26,8 @@ const navLinks = [
 
 
 const Header = () => {
-  return <header className='header flex items-center'>
+  return (
+  <header className='header flex items-center'>
     <div className='container'>
       <div className='flex items-center justify-between'>
        {/* ====== logo ====== */}
@@ -32,18 +35,39 @@ const Header = () => {
   <img src={logo} alt="" />
 </div>
 
-
-
 {/*  ====== menu ====== */}
 <div className='navigation'>
   <ul className='menu flex items-center gap-[2.7rem]'>
-
+{
+navLinks.map((link,index)=> <li key={index}>
+<NavLink to={link.path} className={navClass => 
+      navClass.isActive 
+      ? 'text-primaryColor text-[16px] leading-7 font-[600]' 
+      : 'text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor'} >{link.
+      display} </NavLink>
+  </li>)
+  } 
   </ul>
 </div>
+
+
+{/*======== nav right ========*/}
+
+<div className="flex items-center gap-4">
+
+  <div>
+    <Link to='/'>
+      <figure className='w-[35px] h-[35px] rounded-full'>
+        <img src={userImg} className='w-full rounded-full' alt="" />
+      </figure>
+    </Link>
+  </div>
+</div>
+
       </div>
     </div>
   </header>
-  
+);
 };
 
-export default Header
+export default Header;
