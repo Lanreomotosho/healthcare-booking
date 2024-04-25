144f901e6de2ba7e473b76e3f530dd6c725a8d85
Lanre Omotosho <omotoshoolanrewaju@gmail.com>
2024-04-25 01:11:06 +0100

    first commit

---
+++ Frontend/src/assets/data/doctors.js
@@ -0,0 +1,47 @@
+import doctorImg01 from "../images/doctor-img01.png";
+import doctorImg02 from "../images/doctor-img02.png";
+import doctorImg03 from "../images/doctor-img03.png";
+
+export const doctors = [
+  {
+    id: "01",
+    name: "Dr. Samuel Adeniji",
+    specialty: "Emergency physician",
+    avgRating: 4.9,
+    totalRating: 289,
+    photo: Dr.male2,
+    totalPatients: 2600,
+    hospital: "The Johns Hopkins Hospital.",
+  },
+  {
+    id: "02",
+    name: "Dr. Victoria ",
+    specialty: "Neurologist",
+    avgRating: 4.8,
+    totalRating: 272,
+    photo: Dr.Female2,
+    totalPatients: 1200,
+    hospital: "Massachusetts General Hospital",
+  },
+  {
+    id: "03",
+    name: "Dr. Farid Uddin",
+    specialty: "Dermatologist",
+    avgRating: 4.8,
+    totalRating: 272,
+    photo: Dr.male1,
+    totalPatients: 2500,
+    hospital: "The Mount Sinai Hospital",
+  },
+  {
+    id: "03",
+    name: "Dr. Farid Uddin",
+    specialty: "Critical Care Nurse Practitioner",
+    avgRating: 4.8,
+    totalRating: 272,
+    photo: Dr.Female1,
+    totalPatients: 1600,
+    hospital: "Hospital of the University of Pennsylvania",
+  },
+];
+
