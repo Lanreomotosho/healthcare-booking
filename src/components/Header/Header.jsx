9443c7918508528cbcc1415da834d44626e6dda4
Lanre Omotosho <omotoshoolanrewaju@gmail.com>
2024-04-25 12:58:15 +0100

    first commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
+import logo from '../../assets/images/logo.png';
+import { NavLink, Link } from 'react-router-dom';
+
+
+const navLinks = [
+  {
+    path:'/home',
+    display:'Home'
+  },
+  {
+    path:'/doctor',
+    display:'Find a Doctor'
+  },
+  {
+    path:'/services',
+    display:'Services'
+  },
+  {
+    path:'/contact',
+    display:'Contact'
+  },
+]
+
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  return <header className='header flex items-center'>
+    <div className='container'>
+      <div className='flex items-center justify-between'>
+       {/* ====== logo ====== */}
+<div>
+  <img src={logo} alt="" />
+</div>
+
+
+
+{/*  ====== menu ====== */}
+<div className='navigation'>
+  <ul className='menu flex items-center gap-[2.7rem]'>
+
+  </ul>
+</div>
+      </div>
+    </div>
+  </header>
+  
+};
 
 export default Header
--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -5,21 +5,21 @@ import Login from '../pages/Login';
 import Signup from '../pages/Signup';
 import Contact from '../pages/Contact';
 import Doctors from '../pages/Doctors/Doctors';
-import DoctorsDetails from '../pages/Doctors/DoctorsDetails';
+import DoctorDetails from '../pages/Doctors/DoctorsDetails';
 
 
 
 import {Routes, Route} from 'react-router-dom'
 const Routers = () => {
   return <Routes>
-    <Routes path="/" element={<Home/>} />
-    <Routes path="/home" element={<Home/>} />
-    <Routes path="/doctors" element={<Doctors/>} />
-    <Routes path="/doctors/:id" element={<DonctorDetails/>} />
-    <Routes path="/login" element={<Login/>} />
-    <Routes path="/register" element={<Signup/>} />
-    <Routes path="/contact" element={<Contact/>} />
-    <Routes path="/services" element={<Services/>} />
+    <Route path="/" element={<Home/>} />
+    <Route path="/home" element={<Home/>} />
+    <Route path="/doctors" element={<Doctors/>} />
+    <Route path="/doctors/:id" element={<DoctorDetails/>} />
+    <Route path="/login" element={<Login/>} />
+    <Route path="/register" element={<Signup/>} />
+    <Route path="/contact" element={<Contact/>} />
+    <Route path="/services" element={<Services/>} />
   </Routes>
 };
 
