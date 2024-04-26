ec7866783e73639f1d2d031e8d7838d6bc911e1c
Lanre Omotosho <omotoshoolanrewaju@gmail.com>
2024-04-26 12:26:40 +0100

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+
+import heroImg01 from '../assets/images/hero-img01.png';
 
 const Home = () => {
   return <>
@@ -15,16 +17,66 @@ const Home = () => {
       <div>
         <div className='lg:w-[570px]'>
           <h1 className='text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px]
-          md:leading-[70px]'>We help patients live a healthy, longer life.</h1>
-          <P className='text__para'>  
+          md:leading-[70px]'>
+            We help patients live a healthy, longer life.
+            </h1>
+          <p className='text__para'>
             My name is lanreomonfnkfj ndbdjdkdkd hfjkkdkdjdndnfjfnfknfknfc jjddjcndkvnvknvdmkvnv jndkndkdnkd
-            hdfhfjdkdkddkvnkvnvknvknvd  jndvknvd 
-          </P>
+            hdfhfjdkdkddkvnkvnvknvknvd
+          </p>
+
+          <button className='btn'>
+           Book an Appointment
+          </button>
         </div>
 
+{/* ====== hero counter =======*/}
+<div className='mt-[30px] lg:mt-[70px] flex  flex-col lg:flex-row lg:items-center gap-5 
+lg:gap-[30px]'>
+
+<div>
+  <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700]
+  text-headingColor'>
+40+
+  </h2>
+<span className='w-[100px] h-2 bg-purple-800 rounded-full block mt-[-14px]'>
+</span>
+<p className='text__para'>Years of Experience</p>
+</div>
+
+
+<div>
+  <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700]
+  text-headingColor'>
+15+
+  </h2>
+<span className='w-[100px] h-2 bg-irisBlueColor rounded-full block mt-[-14px]'>
+</span>
+<p className='text__para'>Years of Experience</p>
+</div>
+
+
+<div>
+  <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700]
+  text-headingColor'>
+100%
+  </h2>
+<span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]'>
+</span>
+<p className='text__para'>Patient Satisfaction</p>
+</div>
+</div>
       </div>
 
+{/* ======= hero content ========= */ }
 
+<div className='flex gap-[30px] justify-end'>
+</div>
+<img className='w-full'src={heroImg01} alt="" />
+    </div>
+
+    <div className='mt-[30px]'>
+      <img src={heroIM} alt="" />
     </div>
   </div>
 </section>
