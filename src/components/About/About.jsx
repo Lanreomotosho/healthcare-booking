8cdf6a5c0c506eaa9b34db4a8b31ac2edef18ac5
Lanre Omotosho <omotoshoolanrewaju@gmail.com>
2024-04-30 23:56:48 +0100

    first commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -29,6 +29,7 @@ const About = () => {
 
   <p className='text__para mt-[30px]'>Our best is something we strive for each day, caring for 
   our patients-not looking back at what we accomplished but towards what we can do tomorrow. Providing the best. 
+  Would you like to know more about us?
   </p>
  
   <Link to='/'>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -165,10 +165,30 @@ hover:border-none">
 </section>
 
 <About />
+
+
+{/*======== services section ==========*/}
+<section>
+  <div className='container'>
+    <div className='xl:w-[470px] mx-auto'>
+      <h2 className='heading text-center'>Our medical services
+      </h2>
+      <p className='text__para text-center'>
+        World-class care for everyone. Our health System offers unmatched, expert health care.
+      </p>
+    </div>
+  </div>
+</section>
+
+
+
+
+
+{/*======== services section end ==========*/}
     </>
  
   );
 };
 
 
-export default Home
+export default Home;
