1bd20057b95589c8143ca03321659600d12f3a60
Lanre Omotosho <omotoshoolanrewaju@gmail.com>
2024-05-02 03:04:24 +0100

    first commit

--- src/components/Services/ServiceCard.jsx
+++ src/components/Services/ServiceCard.jsx
@@ -23,12 +23,17 @@ border-[]#181A1E] mt-[30px] mx-auto flex items-center justify-center  group hove
 hover:border-none">
 <BsArrowRight className='group-hover:text-white w-6 h-5'/>
 </Link>
+<span 
+className='w-[44px] h-[44px] flex items-center justify-center texy-[18px] leading-[30px] font-[600]'
+style={{
+  background: `${bgColor}`,
+  color: `${textColor}`,
+  borderRadius: "6px 0 0 6px",
+}}
+>
+{index + 1}
+</span>
 </div>
-
-
-
-
-
     </div>
   );
 };
