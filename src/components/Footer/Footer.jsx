5ae45ea3891198b9f020012b6d4ea8a0b182dd66
Lanre Omotosho <omotoshoolanrewaju@gmail.com>
2024-05-07 13:55:31 +0100

    first commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,105 @@
-import React from 'react'
+import React from 'react';
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/logo.png'
+import { RiLinkedinFill } from 'react-icons/ri';
+import { AiFillGithub, AiOutlineInstagram, AiOutlineFacebook } from 'react-icons/ai';
 
-export default Footer
+const socialLinks = [
+  {
+    path: "https://www.linkedin.com/in/lanreomotosho/",
+    icon: <RiLinkedinFill className='group-hover:text-white w-4 h5' />
+  },
+  {
+    path: "https://github.com/Lanreomotosho/",
+    icon: <AiFillGithub className='group-hover:text-white w-4 h5' />
+  },
+  {
+    path: "https://web.facebook.com/olanrewaju.omotosho.12/",
+    icon: <AiOutlineFacebook className='group-hover:text-white w-4 h5' />
+  },
+  {
+    path: "https://www.instagram.com/lanre__omotosho/",
+    icon: <AiOutlineInstagram className='group-hover:text-white w-4 h5' />
+  },
+];
+
+const quickLinks01 = [
+  {
+    path: '/home',
+    display: "home",
+  },
+  {
+    path: '/',
+    display: "About Us",
+  },
+  {
+    path: '/Services',
+    display: "Services",
+  },
+  {
+    path: '/',
+    display: "Blog",
+  },
+];
+
+  const quickLinks02 = [
+    {
+      path: 'find-a-doctor/',
+      display: "find-a-doctor",
+    },
+    {
+      path: '/',
+      display: "Request an Appointment",
+    },
+    {
+      path: '/',
+      display: "Find a Location",
+    },
+    {
+      path: '/',
+      display: "Get an Opinion",
+    },
+  ];
+
+
+    const quickLinks03 = [
+      {
+        path: '/',
+        display: "Donate",
+      },
+      {
+        path: '/Contact',
+        display: "Contact Us",
+      },
+  ];
+  
+  const Footer = () => {
+
+    const year = new Date().getFullYear()
+
+    return (
+    <footer className='pb-16 pt-10'>
+<div className='container'>
+  <div className=' flex justify-between flex-col md:flex-row flex-wrap gap-[30px]'>
+    <div>
+<img src={logo} alt="" />
+<p className='text-[16px] leading-7 front-[400]'>Copyright {year} developed by Lanre Omotosho  all right reserved.
+ </p>
+
+ <div>
+  {socialLinks.map((link, index) => <Link to={link.path}
+  key={index}>
+{link.icon}
+  </Link>)}
+ </div>
+    </div>
+  </div>
+</div>
+    </footer>
+
+    );
+  };
+
+export default Footer;
+
--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -8,7 +8,6 @@ import { HiStar } from 'react-icons/hi';
 
 const Testimonial = () => {
   return (
-
     <div className='mt-[30px] lg:mt-[55px]'>
         <Swiper 
         modules={[Pagination]} 
@@ -54,9 +53,57 @@ const Testimonial = () => {
                     </p>
                 </div>
             </SwiperSlide>
-            
-        </Swiper>
 
+            <SwiperSlide>
+                <div className='py-[30px] px-5 rounded-3'>
+                    <div className='flex items-center gap-[13px]'>
+                        <img src={patientAvatar} alt="" />
+                        <div>
+                            <h4 className='text-[18px] leading-[30px] font-semibold text-headingColor'>
+                                Samuel Adeniji
+                            </h4>
+                            <div className='flex items-center gap-[2px]'>
+                                <HiStar className='text-yellowColor w-[18px] h-5' />
+                                <HiStar className='text-yellowColor w-[18px] h-5' />
+                                <HiStar className='text-yellowColor w-[18px] h-5' />
+                                <HiStar className='text-yellowColor w-[18px] h-5' />
+                                <HiStar className='text-yellowColor w-[18px] h-5' />
+                            </div>
+                        </div>
+                    </div>
+
+                    <p className='text-[16px] leading-7 mt-4 text-textColor font-[400]'>
+                        "I have taken medical services from them.
+                        They treat so well and they are providing the best mediscal services."
+                    </p>
+                </div>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <div className='py-[30px] px-5 rounded-3'>
+                    <div className='flex items-center gap-[13px]'>
+                        <img src={patientAvatar} alt="" />
+                        <div>
+                            <h4 className='text-[18px] leading-[30px] font-semibold text-headingColor'>
+                               John Victor
+                            </h4>
+                            <div className='flex items-center gap-[2px]'>
+                                <HiStar className='text-yellowColor w-[18px] h-5' />
+                                <HiStar className='text-yellowColor w-[18px] h-5' />
+                                <HiStar className='text-yellowColor w-[18px] h-5' />
+                                <HiStar className='text-yellowColor w-[18px] h-5' />
+                                <HiStar className='text-yellowColor w-[18px] h-5' />
+                            </div>
+                        </div>
+                    </div>
+
+                    <p className='text-[16px] leading-7 mt-4 text-textColor font-[400]'>
+                        "I have taken medical services from them.
+                        They treat so well and they are providing the best mediscal services."
+                    </p>
+                </div>
+            </SwiperSlide>
+        </Swiper>
     </div>
   )
 }
