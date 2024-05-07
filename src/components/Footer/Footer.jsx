d3c33c4d714157db424371b201b7c3a8daf15c06
Lanre Omotosho <omotoshoolanrewaju@gmail.com>
2024-05-07 14:09:45 +0100

    first commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -84,16 +84,29 @@ const quickLinks01 = [
   <div className=' flex justify-between flex-col md:flex-row flex-wrap gap-[30px]'>
     <div>
 <img src={logo} alt="" />
-<p className='text-[16px] leading-7 front-[400]'>Copyright {year} developed by Lanre Omotosho  all right reserved.
+<p className='text-[16px] leading-7 font-[400] text-textColor mt-4'>Copyright {year} developed by Lanre Omotosho  all right reserved.
  </p>
 
- <div>
-  {socialLinks.map((link, index) => <Link to={link.path}
-  key={index}>
+ <div className='flex items-center gap-3 mt-4'>
+  {socialLinks.map((link, index) => 
+  <Link to={link.path} 
+  key={index} className='w-9 h-9  border border-solid border-[#181A1E] rounded-full flex items-center
+  justify-center group hover:bg-primaryColor hover:border-none'>
 {link.icon}
   </Link>)}
  </div>
-    </div>
+ </div>
+
+ <div>
+  <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>
+    Quick Links
+  </h2>
+
+  <ul>
+    {quickLinks01.map((item,index)=> <li key={index} className='mb-4'>
+      <Link to={item.path}>{item.display}</Link></li>)}
+  </ul>
+ </div>
   </div>
 </div>
     </footer>
